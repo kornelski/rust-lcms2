468312679c78ea2136ae07e548c7abf0ddde8d19
Kornel <kornel@geekhood.net>
2017-06-12 15:45:31 +0100

    Lab

--- src/ext.rs
+++ src/ext.rs
@@ -54,23 +54,32 @@ pub trait CIELabExt: Sized {
     /// BFD delta E metric.
     fn bfd_delta_e(&self, other: &CIELab) -> f64;
 
-    // The dE76 metric value.
-    //
-    // The L\*a\*b\* color space was devised in 1976 and, at the same time delta-E 1976 (dE76) came into being.
-    // If you can imagine attaching a string to a color point in 3D Lab space, dE76 describes the sphere that is described by all the possible directions you could pull the string.
-    // If you hear people speak of just plain 'delta-E' they are probably referring to dE76. It is also known as dE-Lab and dE-ab.
-    //
-    // One problem with dE76 is that Lab itself is not 'perceptually uniform' as its creators had intended.
-    // So different amounts of visual color shift in different color areas of Lab might have the same dE76 number.
-    // Conversely, the same amount of color shift might result in different dE76 values.
-    // Another issue is that the eye is most sensitive to hue differences, then chroma and finally lightness and dE76 does not take this into account.
+    /// The dE76 metric value.
+    ///
+    /// The L\*a\*b\* color space was devised in 1976 and, at the same time delta-E 1976 (dE76) came into being.
+    /// If you can imagine attaching a string to a color point in 3D Lab space, dE76 describes the sphere that is described by all the possible directions you could pull the string.
+    /// If you hear people speak of just plain 'delta-E' they are probably referring to dE76. It is also known as dE-Lab and dE-ab.
+    ///
+    /// One problem with dE76 is that Lab itself is not 'perceptually uniform' as its creators had intended.
+    /// So different amounts of visual color shift in different color areas of Lab might have the same dE76 number.
+    /// Conversely, the same amount of color shift might result in different dE76 values.
+    /// Another issue is that the eye is most sensitive to hue differences, then chroma and finally lightness and dE76 does not take this into account.
     fn delta_e(&self, other: &CIELab) -> f64;
 
-    // In 1984 the CMC (Colour Measurement Committee of the Society of Dyes and Colourists of Great Britain) developed and adopted an equation based on LCH numbers.
-    //
-    // Intended for the textiles industry, CMC l:c allows the setting of lightness (l) and chroma (c) factors. As the eye is more sensitive to chroma, the default ratio for l:c is 2:1 allowing for 2x the difference in lightness than chroma (numbers). There is also a 'commercial factor' (cf) which allows an overall varying of the size of the tolerance region according to accuracy requirements. A cf=1.0 means that a delta-E CMC value <1.0 is acceptable.
-    // CMC l:c is designed to be used with D65 and the CIE Supplementary Observer. Commonly-used values for l:c are 2:1 for acceptability and 1:1 for the threshold of imperceptibility.
+    /// In 1984 the CMC (Colour Measurement Committee of the Society of Dyes and Colourists of Great Britain) developed and adopted an equation based on LCH numbers.
+    ///
+    /// Intended for the textiles industry, CMC l:c allows the setting of lightness (l) and chroma (c) factors. As the eye is more sensitive to chroma, the default ratio for l:c is 2:1 allowing for 2x the difference in lightness than chroma (numbers). There is also a 'commercial factor' (cf) which allows an overall varying of the size of the tolerance region according to accuracy requirements. A cf=1.0 means that a delta-E CMC value <1.0 is acceptable.
+    /// CMC l:c is designed to be used with D65 and the CIE Supplementary Observer. Commonly-used values for l:c are 2:1 for acceptability and 1:1 for the threshold of imperceptibility.
     fn cmc_delta_e(&self, other: &CIELab, k: f64, c: f64) -> f64;
+
+    /// amin, amax, bmin, bmax: boundaries of gamut rectangle
+    fn desaturate(&mut self, amin: f64, amax: f64, bmin: f64, bmax: f64) -> bool;
+
+    /// Encodes a Lab value, from a CIELab value to ICC v4 convention.
+    fn encoded(&self) -> [u16; 3];
+
+    /// Encodes a Lab value, from a CIELab value to ICC v2 convention.
+    fn encoded_v2(&self) -> [u16; 3];
 }
 
 impl CIELabExt for CIELab {
@@ -102,6 +111,28 @@ impl CIELabExt for CIELab {
         unsafe {
             ffi::cmsCMCdeltaE(self, other, k, c)
         }
+    }
+
+    fn desaturate(&mut self, amin: f64, amax: f64, bmin: f64, bmax: f64) -> bool {
+        unsafe {
+            0 != ffi::cmsDesaturateLab(self, amax, amin, bmax, bmin)
+        }
+    }
+
+    fn encoded(&self) -> [u16; 3] {
+        let mut out = [0u16; 3];
+        unsafe {
+            ffi::cmsFloat2LabEncoded(out.as_mut_ptr(), self)
+        }
+        out
+    }
+
+    fn encoded_v2(&self) -> [u16; 3] {
+        let mut out = [0u16; 3];
+        unsafe {
+            ffi::cmsFloat2LabEncodedV2(out.as_mut_ptr(), self)
+        }
+        out
     }
 }
 
